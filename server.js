f2b46358ec3a4bcabc0effb7fb68ebc0a77f5514
Joshua Ibrom <joshuaibrom@gmail.com>
2022-07-27 19:06:42 -0500

    Basic rendering of index page

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const port = 5000;
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "static")));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(port, () => console.log(`Started listening on ${port}`));
